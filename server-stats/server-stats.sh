6027d0f16e2a8f9768b7571b7b02d0774e3b6d6a
wegoagain <tsdevelops00@gmail.com>
2025-05-25 00:37:22 +0100

    updated

--- server-stats/server-stats.sh
+++ server-stats/server-stats.sh
@@ -16,6 +16,16 @@ echo DISK USAGE
 df -h | grep '/dev/nvme0n1p6' | awk '{print "Total disk usage: Used: "$3", Free:"$4", Percentage: "$5""}'
 }
 
+cpu_top5(){
+echo TOP 5 CPU USAGE
+ps -eo pid,comm,%cpu --sort=-%cpu | head -n 6
+}
+
+mem_top5(){
+echo TOP 5 MEM USAGE
+ps -eo pid,comm,%cpu --sort=-%cpu | head -n 6
+}
+
 
 echo "welcome to my server stats"
 echo ""
@@ -25,4 +35,9 @@ memory_usage
 echo ""
 data_usage
 echo ""
+cpu_top5
+echo ""
+mem_top5
+
+
 
